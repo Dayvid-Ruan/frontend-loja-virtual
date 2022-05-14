822d0c17b8de67fc1e3e23712bf3e397414e888a
Dayvid <ruan12bbg@gmail.com>
2022-05-14 09:50:51 -0300

    frontend implementado

---
+++ frontend/src/components/navBarAdmin/navBarAdmin.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./NavBarAdmin.css";
+
+function NavBarAdmin () {
+  const userInfo = JSON.parse(localStorage.getItem("admin"));
+  return (
+    <div>
+      <nav className="navAdmin">
+        <div className="adminName">
+          <p>{ userInfo.name }</p>
+        </div>  
+        <div className="divAdmin">
+          <div className="logoutAdmin">
+            <p>
+              <a className="linkAdmin" href="/" onClick={ () => localStorage.removeItem("user") }>
+            Sair
+              </a>
+            </p>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default NavBarAdmin;
